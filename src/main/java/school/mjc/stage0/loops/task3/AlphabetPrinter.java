35fbb37020f6b2f39add3e0af573c9b56eb66845
mymindinsane <arturguletskiy21@gmail.com>
2024-06-20 13:37:37 +0300

    done AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char ch;
+        for (int i = 97;i <= 122;i++){
+            ch = (char) i;
+            System.out.println(ch);
+        }
     }
 }
