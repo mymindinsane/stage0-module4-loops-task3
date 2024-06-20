0febf1b12edd3519b2e9ba6392f084794a06995b
mymindinsane <arturguletskiy21@gmail.com>
2024-06-20 17:22:31 +0300

    done DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
+    int sum = 0;
     public void printDigitsSum(int t){
-
+        String empty = "";
+        int length = (t + empty).length();
+        for (int i = 0;i < length;i++){
+            sum += Math.abs(t) % (int)(Math.pow(10,i + 1)) / Math.pow(10, i) ;
+        }
+        System.out.println(sum);
     }
 }
