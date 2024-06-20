c1f68edfb893d8297af95676a2e180ccbd8a06ed
mymindinsane <arturguletskiy21@gmail.com>
2024-06-20 13:33:03 +0300

    done RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int sum = 0;
+        int max = Math.max(firstBoarder,secondBoarder);
+        int min = (Math.min(firstBoarder,secondBoarder));
+        for (int i = min;i <= max;i++){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
