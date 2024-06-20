12e5339ab8a0ccf079e31a679e638c831e8d4ad7
mymindinsane <arturguletskiy21@gmail.com>
2024-06-20 17:03:43 +0300

    done NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
+    int sum = 0;
+    int currentNumber = 0;
     public void calculateSum(int lengthOfLastNumber) {
-
+        for (int i = 0;i < lengthOfLastNumber;i++){
+            currentNumber = 9 + (10 * currentNumber);
+            sum += currentNumber;
+        }
+        System.out.println(sum);
     }
 }
